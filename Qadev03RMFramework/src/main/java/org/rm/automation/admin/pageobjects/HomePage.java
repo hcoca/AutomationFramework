e8a117ce7ae7d519db4d5002b9ce5de716f4278c
AlejandraGareca <ale.paogirl@gmail.com>
2015-09-13 20:42:36 -0400

    Log message added to HomePage

--- Qadev03RMFramework/src/main/java/org/rm/automation/admin/pageobjects/HomePage.java
+++ Qadev03RMFramework/src/main/java/org/rm/automation/admin/pageobjects/HomePage.java
@@ -3,21 +3,25 @@ package org.rm.automation.admin.pageobjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.rm.automation.admin.pageobjects.LoginPage;
+import org.rm.automation.utils.LogManager;
 
 public class HomePage extends NavigationBarPage{	
-	private By signOutBtn = By.xpath("//span[contains(.,'sign out')]");
+	@FindBy(xpath="//span[contains(.,'sign out')]")
+	  WebElement signOutBtn;
 
-	/*---------------------------------------*/
 	public HomePage(WebDriver driver) {
 		super(driver);
-		
+		PageFactory.initElements(driver, this);		
 	}
 	public LoginPage SignOut() {
-		WebElement element=driver.findElement(signOutBtn);
-		if(element.isDisplayed()||element.isEnabled())
-			element.click();
+		if(signOutBtn.isDisplayed()||signOutBtn.isEnabled())
+		{
+			LogManager.info("HomePage: Logging out of the application");
+			signOutBtn.click();
+		}
 		return new LoginPage(driver);
 	}
 }
