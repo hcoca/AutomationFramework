18c80e4206953aa6c42c2471d6f8929dd5b56073
AlejandraGareca <ale.paogirl@gmail.com>
2015-09-18 16:17:16 -0400

    last test case of location feature and some improvements

--- Qadev03RMFramework/src/main/java/org/rm/automation/admin/pageobjects/locations/RemoveLocationsPage.java
+++ Qadev03RMFramework/src/main/java/org/rm/automation/admin/pageobjects/locations/RemoveLocationsPage.java
@@ -11,22 +11,47 @@ import org.rm.automation.utils.Waiters;
 import org.testng.Assert;
 public class RemoveLocationsPage {
 	private WebDriver driver;
+	private By confRoomColumn;
 	@FindBy(xpath="//button[@ng-click='cancel()']")
 	  WebElement cancelBtn;
 	@FindBy(xpath="//button[@ng-click='removeLocations()']")
 	  WebElement removeBtn;
+	
+	
 	public RemoveLocationsPage(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
-	public HomePage confirmRemoveLocation() {
+	private boolean verifyConferenceAssociated(String roomName) {
+		LogManager.info("RemoveLocationsPage: Verifying conference room associated");
+		confRoomColumn = By.xpath("//span[@class='ng-binding' and contains(.,'"+roomName+"')]");
+		WebElement element = driver.findElement(confRoomColumn);
+		if(element.isDisplayed())
+		{
+			return true;
+		}
+		else
+			return false;
+	}
+	public LocationsPage confirmRemoveLocation(String roomName) {
+		Assert.assertTrue(verifyConferenceAssociated(roomName));
 		Waiters.WaitByXPath("//button[@ng-click='removeLocations()']", driver);
 		if(removeBtn.isDisplayed()||removeBtn.isEnabled())
 		{
-			LogManager.info("LocationsPage: Click on Remove button to confirm remove location");
+			LogManager.info("RemoveLocationsPage: Click on Remove button to confirm remove location");
 			removeBtn.click();
 		}
-		return new HomePage(driver);
+		return new LocationsPage(driver);
+	}
+	
+	public LocationsPage confirmRemoveLocation() {
+		Waiters.WaitByXPath("//button[@ng-click='removeLocations()']", driver);
+		if(removeBtn.isDisplayed()||removeBtn.isEnabled())
+		{
+			LogManager.info("RemoveLocationsPage: Click on Remove button to confirm remove location");
+			removeBtn.click();
+		}
+		return new LocationsPage(driver);
 	}
 
 }
