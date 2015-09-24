c218073544e7ef527026c50e97f63ebba7f779f1
vanevane <vane.27.04@hotmail.com>
2015-09-24 11:03:29 -0400

    VerifyRoomsDisplayed test case

--- Qadev03RMFramework/src/test/java/org/rm/automation/tablet/tests/search/VerifyRoomsDisplayed.java
+++ Qadev03RMFramework/src/test/java/org/rm/automation/tablet/tests/search/VerifyRoomsDisplayed.java
@@ -1,76 +1,49 @@
 package org.rm.automation.tablet.tests.search;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
-//import org.rm.automation.admin.pageobjects.HomePage;
-//import org.rm.automation.admin.pageobjects.LoginPage;
-//import org.rm.automation.admin.pageobjects.locations.IssuesPage;
-//import org.rm.automation.admin.pageobjects.resources.AddResourcesPage;
-//import org.rm.automation.admin.pageobjects.resources.ResourcesPage;
+import org.json.simple.JSONObject;
 import org.rm.automation.base.TestBaseSetup;
 import org.rm.automation.tablet.pageobjects.LoginPage;
+import org.rm.automation.tablet.pageobjects.homepage.HomePage;
+import org.rm.automation.tablet.pageobjects.search.SearchPage;
 import org.rm.automation.utils.LogManager;
 import org.rm.automation.utils.ReadPropertyValues;
-import org.rm.automation.utils.StringGenerator;
+import org.rm.automation.utils.api.ConferenceRoomsRequests;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class VerifyRoomsDisplayed extends TestBaseSetup{
+public class VerifyRoomsDisplayed extends TestBaseSetup {
 	Properties settings = ReadPropertyValues
 			.getPropertyFile("./Config/settings.properties");
 	private String username = settings.getProperty("username");
 	private String password = settings.getProperty("password");
-	private String url = "http://" + settings.getProperty("server")+":"+
+	private String url = "http://" + settings.getProperty("server") + ":" +
 							settings.getProperty("port");
-	
-	private String name = StringGenerator.getString();
-	private String displayName = StringGenerator.getString();
-	private String iconName = "fa-gears";
+	private String roomName;
 	
 	private LoginPage loginPage;
-//	private HomePage homePage;
-//	private ResourcesPage resourcesPage;
-//	private AddResourcesPage addResourcesPage;
-//	private IssuesPage issuesPage;
+	private HomePage homePage;
+	private SearchPage searchPage;
 	
-	@Test
-	public void testSearchMeetingByRoom()
+	@BeforeMethod
+	public void Preconditions() throws UnsupportedOperationException, IOException
 	{
-		LogManager.info("SearchMeetingByRoom: Executing Test Case");
-
-//		String roomName = "Morochata";
-//		String url = "http://172.20.208.105:4040";
-//		String username = "atxrm\\elver";
-//		String password = "Control123";
-//		BeginTest.getLogin()
-//		.setUrl(url)
-//		.setUsername(username)
-//		.setPassword(password)
-//		.login()
-//		.setRoom()
-//		.access()
-//		.selectSearchPage()
-//		.enableAdvancedSearch()
-//		.typeRoomName(roomName)
-//		.selectRoom(roomName)
-//		.isTheRightRoom(roomName);
-		
-		new LoginPage(driver)
-		.access(url, username, password, "RNY Room 1")
-		.selectSearchPage()
-		.verifyRoomsDisplayed();
-		
-//		loginPage = new LoginPage(driver);
-//  		homePage = loginPage.SignIn(username, password);
-//  		resourcesPage = homePage.SelectResourcesOption();		  		
-//		addResourcesPage = resourcesPage.AddResource()
-//				.setName(name)
-//				.setDisplayName(displayName)
-//				.setIcon(iconName);
-//		resourcesPage =	addResourcesPage.Save();		
-//		issuesPage = resourcesPage.SelectIssuesOption();
-//		resourcesPage = issuesPage.SelectResourcesOption()
-//				.VerifyResourceWasCreated(name, displayName, iconName);
-//		resourcesPage.SignOut();
+		LogManager.info("SearchByRoomName: Executing Precondition, creating a resource");
+		ArrayList<JSONObject> list = ConferenceRoomsRequests.getRooms();
+		roomName = list.get(0).get("customDisplayName").toString();
 	}
 
+	@Test
+	public void testVerifyRoomsDisplayed()
+	{
+		LogManager.info("VerifyRoomsDisplayed: Executing Test Case");
+
+		loginPage = new LoginPage(driver);
+		homePage = loginPage.access(url, username, password, roomName);
+		searchPage = homePage.selectSearchPage()
+				.verifyRoomsDisplayed();
+	}
 }
