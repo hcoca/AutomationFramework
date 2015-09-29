940495e5b4c34adf97e49ee7c490f43e7bfd3949
luis fernando cachi <lufer_cc@hotmail.com>
2015-09-28 23:28:42 -0400

    add EmailServerPageLocator

---
+++ Qadev03RMFramework/src/main/java/org/rm/automation/admin/locators/emailServers/RemoveServerPageLocator.java
@@ -0,0 +1,7 @@
+package org.rm.automation.admin.locators.emailServers;
+
+public class RemoveServerPageLocator {
+	  public static final String popdeleteLocator = "//div[2]/div/div/span";
+	  public static final String btnyesdeleteLocator = "//button[@ng-click='onYes()']";
+	  public static final String btnnodeleteLocator = "//button[@ng-click='onNo()']";
+}
