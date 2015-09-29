454a7eac856980f20fd5abe0755158c3779ca242
AlejandraGareca <ale.paogirl@gmail.com>
2015-09-28 20:03:19 -0400

    locations page objects fixed

---
+++ Qadev03RMFramework/src/main/java/org/rm/automation/admin/locators/locations/FormAssociateRoomPageLocators.java
@@ -0,0 +1,7 @@
+package org.rm.automation.admin.locators.locations;
+
+public class FormAssociateRoomPageLocators {
+	public static final String searchTextBoxPath = "//input[@ng-model='searchDisplayName']";
+	public static final String addRoomBtnPath = "//button[@ng-click='assignRoom(room)']";
+	public static final String infoLinkPath = "//a[contains(.,'Location Info')]";
+}
--- Qadev03RMFramework/src/main/java/org/rm/automation/admin/pageobjects/locations/RemoveLocationsPage.java
+++ Qadev03RMFramework/src/main/java/org/rm/automation/admin/pageobjects/locations/RemoveLocationsPage.java
@@ -4,15 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.rm.automation.utils.LogManager;
+import org.rm.automation.admin.locators.locations.RemoveLocationsPageLocators;
 import org.rm.automation.utils.Waiters;
 import org.testng.Assert;
 public class RemoveLocationsPage {
 	private WebDriver driver;
 	private By confRoomColumn;
-	/*@FindBy(xpath="//button[@ng-click='cancel()']")
-	  WebElement cancelBtn;*/
-	@FindBy(xpath="//button[@ng-click='removeLocations()']")
+	@FindBy(xpath=RemoveLocationsPageLocators.cancelBtnPath)
+	  WebElement cancelBtn;
+	@FindBy(xpath=RemoveLocationsPageLocators.removeBtnPath)
 	  WebElement removeBtn;
 	
 	
@@ -21,7 +21,6 @@ public class RemoveLocationsPage {
 		PageFactory.initElements(driver, this);
 	}
 	private boolean verifyConferenceAssociated(String roomName) {
-		LogManager.info("RemoveLocationsPage: Verifying conference room associated");
 		confRoomColumn = By.xpath("//span[@class='ng-binding' and contains(.,'"+roomName+"')]");
 		WebElement element = driver.findElement(confRoomColumn);
 		if(element.isDisplayed())
@@ -33,20 +32,17 @@ public class RemoveLocationsPage {
 	}
 	public LocationsPage confirmRemoveLocation(String roomName) {
 		Assert.assertTrue(verifyConferenceAssociated(roomName));
-		Waiters.WaitByXPath("//button[@ng-click='removeLocations()']", driver);
+		Waiters.WaitByVisibilityOfWebElement(removeBtn, driver);
 		if(removeBtn.isDisplayed()||removeBtn.isEnabled())
 		{
-			LogManager.info("RemoveLocationsPage: Click on Remove button to confirm remove location");
 			removeBtn.click();
 		}
 		return new LocationsPage(driver);
 	}
 	
 	public LocationsPage confirmRemoveLocation() {
-		Waiters.WaitByXPath("//button[@ng-click='removeLocations()']", driver);
-		if(removeBtn.isDisplayed()||removeBtn.isEnabled())
+		Waiters.WaitByVisibilityOfWebElement(removeBtn, driver);if(removeBtn.isDisplayed()||removeBtn.isEnabled())
 		{
-			LogManager.info("RemoveLocationsPage: Click on Remove button to confirm remove location");
 			removeBtn.click();
 		}
 		return new LocationsPage(driver);
